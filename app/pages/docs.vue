0f418c71871b32d1274bfdccbccac025d7ce157e
Lafllamme <teke900@gmail.com>
2026-03-28 17:37:52 +0100

    feat: micro ux

--- app/pages/docs.vue
+++ app/pages/docs.vue
@@ -468,10 +468,13 @@ onMounted(() => {
               href="https://github.com/openai/codex/issues/14766"
               target="_blank"
               rel="noopener noreferrer"
-              class="relative w-fit inline-flex items-center self-start gap-2 pb-0.5 text-sm after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 after:bg-[#10b981] !text-[#10b981] after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] after:content-[''] hover:after:origin-bottom-left hover:after:scale-x-100"
+              class="group relative w-fit inline-flex items-center self-start gap-2 pb-0.5 text-sm after:absolute after:bottom-0 after:left-0 after:h-[1px] after:w-full after:origin-bottom-right after:scale-x-0 after:bg-[#10b981] !text-[#10b981] after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] after:content-[''] hover:after:origin-bottom-left hover:after:scale-x-100"
             >
               View GitHub Discussion
-              <Icon name="ph:arrow-square-out" class="h-3.5 w-3.5" />
+              <Icon
+                name="ph:arrow-up-right"
+                class="h-3.5 w-3.5 transition-transform duration-200 ease-out group-hover:translate-x-0.5 group-hover:-translate-y-0.5"
+              />
             </a>
           </div>
         </section>
